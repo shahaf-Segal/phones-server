99ec24c64ab44a951854d0b1e2cb0e136c78aaa9
shahaf-Segal <shahaf100@outlook.co.il>
2024-07-03 00:20:39 +0300

    [PA-2] added create Phone

--- controllers/phone.controller.js
+++ controllers/phone.controller.js
@@ -6,8 +6,20 @@ const getAllPhones = async (req, res) => {
     return res.status(200).send(allPhones);
   } catch (error) {
     console.error("ERROR AT getAllPhones:", error);
-    res.status(500).send("ERROR");
+    res.status(500).send("Server Error");
   }
 };
 
-module.exports = { getAllPhones };
+const createPhone = async (req, res) => {
+  try {
+    const newPhone = req.body;
+    if (!newPhone) return res.status(400).send("No phone data sent");
+    const createdPhone = await Phone.create(newPhone);
+    return res.status(200).send(createdPhone);
+  } catch (error) {
+    console.error("ERROR AT createPhone:", error);
+    res.status(500).send("Server Error");
+  }
+};
+
+module.exports = { getAllPhones, createPhone };
